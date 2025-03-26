fef3198d4e61a6600be24b2a79351f6a1d1fc83d
wnddjgld <wnddjgld@gmail.com>
2025-03-26 11:48:17 +0900

    4장 예제

--- src/main/java/kr/ac/kopo/wnddjgld/firstspringboot/controller/Example02Controller.java
+++ src/main/java/kr/ac/kopo/wnddjgld/firstspringboot/controller/Example02Controller.java
@@ -1,4 +1,13 @@
 package kr.ac.kopo.wnddjgld.firstspringboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Example02Controller {
+    @RequestMapping(value = "/exam02", method = RequestMethod.GET)
+    public String requestMethod() {
+        return "@RestController 연습중입니다.";
+    }
 }
--- src/main/java/kr/ac/kopo/wnddjgld/firstspringboot/controller/Example03Controller.java
+++ src/main/java/kr/ac/kopo/wnddjgld/firstspringboot/controller/Example03Controller.java
@@ -1,4 +1,14 @@
 package kr.ac.kopo.wnddjgld.firstspringboot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/test")
 public class Example03Controller {
+    @GetMapping(value = "/exam04")
+    public String requestMethod() {
+        return "requestTest";
+    }
 }
